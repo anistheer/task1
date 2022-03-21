336557eeea26fec9805a5624a10a4905bf23b97a
Sergey Sporshev <zxcser00@gmail.com>
2022-03-21 15:14:54 +0300

    yet another menu fix

---
+++ README.md
@@ -0,0 +1,7 @@
+# **Запуск:**
+
+npm i # установка node_modules
+
+npm run start # запуск Webpack DevServer (localhost:9000)
+
+npm run build; npx serve -s dist # сборка в /dist и запуск сервера (localhost:3000)
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,13 +1,28 @@
+import { debounce } from "./debounce";
+
 const header__burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
 const lang = document.querySelector('.lang');
 const body = document.body;
+
+const mobileBreakpoint = 500;
+const resizes = [];
 
 header__burger.addEventListener('click', () => {
   toggleElementClass(header__burger, 'header__burger_active');
   toggleMenuVisible();
   toggleElementClass(body, 'overflow-hidden');
 });
+
+const onResize = debounce(() => {
+  resizes.push(window.innerWidth)  
+  if (resizes.length > 2) resizes.shift();
+  if (resizes[0] > mobileBreakpoint && resizes[1] < mobileBreakpoint) {
+    hideMenu()
+  }
+}, 200)
+
+window.addEventListener('resize', onResize)
 
 function toggleElementClass(element, className) {
   if (element.classList.contains(className)) {
@@ -20,6 +35,13 @@ function toggleMenuVisible() {
   toggleElementClass(lang, 'hidden');
 }
 
+function hideMenu() {
+  header__burger.classList.remove( 'header__burger_active');
+  menu.classList.add('hidden')
+  lang.classList.add('hidden')
+  body.classList.remove('overflow-hidden')
+}
+
 if (window.innerWidth < 500) {
-  toggleMenuVisible();
+  hideMenu()
 }
