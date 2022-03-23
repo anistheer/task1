85cd96089fdbedff06b62d5d3cf861f6d5721719
Sergey Sporshev <zxcser00@gmail.com>
2022-03-23 17:08:15 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './scss/style.scss';
-import photo from './assets/BSPP2266.jpg';
-import port1 from './assets/portfolio1.jpg';
-import port2 from './assets/portfolio2.jpg';
-import port3 from './assets/portfolio3.jpg';
-import menu from './scripts/menu'
-import headerScroll from './scripts/headerScroll'
+import './assets/BSPP2266.jpg';
+import './assets/portfolio1.jpg';
+import './assets/portfolio2.jpg';
+import './assets/portfolio3.jpg';
+import './scripts/menu'
+import './scripts/headerScroll'
