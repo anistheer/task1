623fc4bdd6f4856ff6f8d01afdaa47ccfd6a1bcd
Sergey Sporshev <zxcser00@gmail.com>
2022-03-24 15:24:46 +0300

    gh-pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './scss/style.scss';
-import './assets/BSPP2266.jpg';
-import './assets/portfolio1.jpg';
-import './assets/portfolio2.jpg';
-import './assets/portfolio3.jpg';
-import './scripts/menu'
-import './scripts/headerScroll'
