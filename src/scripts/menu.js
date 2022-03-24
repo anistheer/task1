bc7ff911bfc37f0e619c6453f154f965e2f38b1e
Sergey Sporshev <zxcser00@gmail.com>
2022-03-24 14:46:44 +0300

    Удалил onResize

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,29 +1,9 @@
-import { debounce } from "./debounce";
 import { MOBILE_BREAKPOINT } from "./variables";
 
 const header__burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
 const lang = document.querySelector('.lang');
 const body = document.body;
-
-const resizes = [ window.innerWidth, window.innerWidth ];
-
-let firstResize = false;
-
-const onResize = debounce(() => {
-  if (!firstResize) {
-    firstResize=true;
-    hideMenu();
-  }
-  resizes.push(window.innerWidth)  
-  resizes.shift();  
-  if ( resizes[0] < MOBILE_BREAKPOINT && resizes[1] > MOBILE_BREAKPOINT) {
-    hideMenu();
-  }
-}, 50)
-
-
-window.addEventListener('resize', onResize)
 
 header__burger.addEventListener('click', () => {
   toggleElementClass(header__burger, 'header__burger_active');
